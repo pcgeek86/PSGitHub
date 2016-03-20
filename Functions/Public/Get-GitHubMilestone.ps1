1afab08b75e08de3f239110a071cacfe15cafc4b
Trevor Sullivan <trevor@trevorsullivan.net>
2016-03-20 14:16:33 -0600

    Implement Get-GitHubMilestone
    Implement Get-GitHubMilestone

--- Functions/Public/Get-GitHubMilestone.ps1
+++ Functions/Public/Get-GitHubMilestone.ps1
@@ -13,6 +13,14 @@ function Get-GitHubMilestone {
     .Parameter Milestone
     The number of the milestone that you want to retrieve. 
     
+    .Example
+    ### Get a specific milestone, based on the milestone's number.
+    Get-GitHubMilestone -Milestone 1;
+    
+    .Example
+    ### Get a list of milestone for the specified repository
+    Get-GitHubMilestone -Owner pcgeek86 -Repository PSGitHub;
+    
     .Link
     https://trevorsullivan.net
     https://developer.github.com/v3/issues    
@@ -24,7 +32,7 @@ function Get-GitHubMilestone {
         [string] $Owner
       , [Parameter(Mandatory = $true)]
         [string] $Repository
-      , [Parameter(ParameterSetName = 'SpecificMilestone', Mandatory = $false)]
+      , [Parameter(ParameterSetName = 'SpecificMilestone', Mandatory = $true)]
         [string] $Milestone
       , [Parameter(ParameterSetName = 'FindMilestones', Mandatory = $false)]
         [ValidateSet('Open', 'Closed', 'All')]
@@ -37,37 +45,51 @@ function Get-GitHubMilestone {
         [string] $Direction
     )
     
-    switch ($Sort) {
-        'DueDate' { $Sort = 'due_on'; break; }
-        'Completeness' { $Sort = 'completeness'; break; }
-        default { break; }
-    }
-    
-    switch ($State) {
-        'Open' {
-            $State = 'open'; break; }
-        'Closed' {
-            $State = 'closed'; break; }
-        'All' {
-            $State = 'all'; break; }
-        default {
-            break; }
-    }
-    
-    switch ($Direction) {
-        'Ascending' {
-            $Direction = 'asc'; break; }
-        'Descending' {
-            $Direction = 'desc'; break; }
-        default {
-            break; }
-    }
-    
     ### Build the core message body -- we'll add more properties soon
     $ApiBody = @{
-        title = $Title;
     };
     
+    ### Add the milestone property
+    if ($Milestone) { $ApiBody.Add('milestone', $Milestone); }
+    
+    ### Normalize the "sort" JSON property
+    if ($Sort) {
+        switch ($Sort) {
+            'DueDate' { $Sort = 'due_on'; break; }
+            'Completeness' { $Sort = 'completeness'; break; }
+            default { break; }
+        }
+        $ApiBody.Add('sort', $Sort);
+    }
+    
+    ### Normalize the "state" JSON property
+    if ($State) {
+        switch ($State) {
+            'Open' {
+                $State = 'open'; break; }
+            'Closed' {
+                $State = 'closed'; break; }
+            'All' {
+                $State = 'all'; break; }
+            default {
+                break; }
+        }
+        $ApiBody.Add('state', $State);
+    }
+    
+    ### Normalize the "direction" JSON property
+    if ($Direction) {
+        switch ($Direction) {
+            'Ascending' {
+                $Direction = 'asc'; break; }
+            'Descending' {
+                $Direction = 'desc'; break; }
+            default {
+                break; }
+        }
+        $ApiBody.Add('direction', $Direction);
+    }
+        
     ### Determine the appropriate REST method to use
     if ($Milestone) {
         $RestMethod = '/repos/{0}/{1}/milestones/{2}' -f $Owner, $Repository, $Milestone; }
