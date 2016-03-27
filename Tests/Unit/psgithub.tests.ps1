3d4eca70f31fbea0a06ff63d654016b69a52178e
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-03-28 11:45:57 +1300

    Minor code tweaks to psgithub.test.ps1.

--- Tests/Unit/psgithub.tests.ps1
+++ Tests/Unit/psgithub.tests.ps1
@@ -4,66 +4,86 @@
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
 
-$Global:ModuleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
+$ModuleRoot = "..\..\$($Script:MyInvocation.MyCommand.Path)"
 
-$OldLocation = Get-Location
-Set-Location -Path $ModuleRoot
-if (Get-Module PSGitHub -All)
-{
-    Get-Module PSGitHub -All | Remove-Module
-}
+Push-Location `
+    -Path $ModuleRoot
+Get-Module `
+    -Name PSGitHub `
+    -All | Remove-Module
 
-Import-Module "$Global:ModuleRoot\PSGitHub.psd1" -Force -DisableNameChecking
-$Global:ArtifactPath = "$Global:ModuleRoot\Artifacts"
-$null = New-Item -Path "$Global:ArtifactPath" -ItemType Directory -Force -ErrorAction SilentlyContinue
+Import-Module `
+    -Name "$ModuleRoot\PSGitHub.psd1" `
+    -Force `
+    -DisableNameChecking
+$Global:ArtifactPath = "$ModuleRoot\Artifacts"
+$null = New-Item `
+    -Path $ArtifactPath `
+    -ItemType Directory `
+    -Force `
+    -ErrorAction SilentlyContinue
 
 # Perform PS Script Analyzer tests on module code only
-$null = Set-PackageSource -Name PSGallery -Trusted -Force
-$null = Install-Module -Name 'PSScriptAnalyzer' -Confirm:$False
-Import-Module -Name 'PSScriptAnalyzer'
+$null = Set-PackageSource `
+    -Name PSGallery `
+    -Trusted `
+    -Force
+$null = Install-Module `
+    -Name PSScriptAnalyzer `
+    -Confirm:$False
+Import-Module `
+    -Name PSScriptAnalyzer
 
 Describe 'PSScriptAnalyzer' {
     Context 'PSGitHub Module, Functions and TabCompleters' {
         It 'Passes Invoke-ScriptAnalyzer' {
             # Perform PSScriptAnalyzer scan.
             $PSScriptAnalyzerResult = Invoke-ScriptAnalyzer `
-                -path "$ModuleRoot\PSGitHub.psm1" `
+                -Path "$ModuleRoot\PSGitHub.psm1" `
                 -Severity Warning `
                 -ErrorAction SilentlyContinue
             $PSScriptAnalyzerResult += Invoke-ScriptAnalyzer `
-                -path "$ModuleRoot\Functions\Public\*.ps1" `
+                -Path "$ModuleRoot\Functions\Public\*.ps1" `
                 -Severity Warning `
                 -ErrorAction SilentlyContinue
             $PSScriptAnalyzerResult += Invoke-ScriptAnalyzer `
                 -path "$ModuleRoot\Functions\Private\*.ps1" `
-                -Severity Warning `
+                -Peverity Warning `
                 -ErrorAction SilentlyContinue
             $PSScriptAnalyzerResult += Invoke-ScriptAnalyzer `
-                -path "$ModuleRoot\TabCompleters\*.ps1" `
+                -Path "$ModuleRoot\TabCompleters\*.ps1" `
                 -Severity Warning `
                 -ErrorAction SilentlyContinue
-            $PSScriptAnalyzerErrors = $PSScriptAnalyzerResult | Where-Object { $_.Severity -eq 'Error' }
-            $PSScriptAnalyzerWarnings = $PSScriptAnalyzerResult | Where-Object { $_.Severity -eq 'Warning' }
+            $PSScriptAnalyzerErrors = $PSScriptAnalyzerResult `
+                | Where-Object { $_.Severity -eq 'Error' }
+            $PSScriptAnalyzerWarnings = $PSScriptAnalyzerResult `
+                | Where-Object { $_.Severity -eq 'Warning' }
             if ($PSScriptAnalyzerErrors -ne $null)
             {
-                Write-Warning -Message 'There are PSScriptAnalyzer errors that need to be fixed:'
-                @($PSScriptAnalyzerErrors).Foreach( { Write-Warning -Message "$($_.Scriptname) (Line $($_.Line)): $($_.Message)" } )
-                Write-Warning -Message  'For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/psscriptAnalyzer/'
+                Write-Warning `
+                    -Message 'There are PSScriptAnalyzer errors that need to be fixed:'
+                @($PSScriptAnalyzerErrors).Foreach( {
+                    Write-Warning -Message "$($_.Scriptname) (Line $($_.Line)): $($_.Message)"
+                } )
+                Write-Warning `
+                    -Message  'For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/psscriptAnalyzer/'
                 $PSScriptAnalyzerErrors.Count | Should Be $null
             }
             if ($PSScriptAnalyzerWarnings -ne $null)
             {
-                Write-Warning -Message 'There are PSScriptAnalyzer warnings that should be fixed:'
-                @($PSScriptAnalyzerWarnings).Foreach( { Write-Warning -Message "$($_.Scriptname) (Line $($_.Line)): $($_.Message)" } )
+                Write-Warning `
+                    -Message 'There are PSScriptAnalyzer warnings that should be fixed if possible:'
+                @($PSScriptAnalyzerWarnings).Foreach( {
+                    Write-Warning -Message "$($_.Scriptname) (Line $($_.Line)): $($_.Message)"
+                } )
             }
         }
     }
 }
 
 InModuleScope PSGitHub {
-
 #region Functions
 #endregion
 }
 
-Set-Location -Path $OldLocation
+Pop-Location
