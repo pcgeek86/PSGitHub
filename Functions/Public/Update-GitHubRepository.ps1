5510233909f705f72eb863d5323829751a9a7385
Felix Becker <felix.b@outlook.com>
2021-09-23 13:12:40 +0200

    Extend Update-GitHubRepository with more settings

--- Functions/Public/Update-GitHubRepository.ps1
+++ Functions/Public/Update-GitHubRepository.ps1
@@ -21,7 +21,7 @@
     .Parameter Name
     The name of the existing GitHub Repository that will be operated on.
 
-    .Parameter NewName
+    .Parameter Rename
     This parameter allows you to specify a new name for the GitHub repository.
 
     .Parameter Description
@@ -39,40 +39,82 @@
     .Notes
     Created by Trevor Sullivan <trevor@trevorsullivan.net>
     #>
+    [CmdletBinding(SupportsShouldProcess)]
     param (
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [string] $Owner,
-        [Parameter(Mandatory)]
-        [string] $Name,
-        [string] $NewName,
+
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string] $RepositoryName,
+
+        [Alias('NewName')]
+        [string] $Rename,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
         [string] $Description,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
         [string] $Homepage,
+
         [Boolean] $DisableIssues,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
         [Boolean] $Private,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string] $DefaultBranch,
+
         # Optional base URL of the GitHub API, for example "https://ghe.mycompany.com/api/v3/" (including the trailing slash).
         # Defaults to "https://api.github.com"
         [Uri] $BaseUri = [Uri]::new('https://api.github.com'),
         [Security.SecureString] $Token = (Get-GitHubToken)
     )
 
-    $Body = @{
-        name = if ($NewName) { $NewName } else { $Name };
-        description = $Description;
-        homepage = $Homepage;
-        private = [bool]$Private;
-        has_issues = [bool]!$DisableIssues;
-        auto_init = [bool]$IncludeReadme;
-    } | ConvertTo-Json;
-    Write-Verbose -Message $Body;
+    process {
+        $body = @{ }
 
-    $ApiCall = @{
-        Uri = 'repos/{0}/{1}' -f $Owner, $Name;
-        Body = $Body;
-        Method = 'Patch';
-        Token = $Token
-        BaseUri = $BaseUri
+        if ($Rename) {
+            $body.name = $Rename
+        }
+        if ($Description) {
+            $body.description = $Description;
+        }
+        if ($Homepage) {
+            $body.homepage = $Homepage;
+        }
+        if ($PSBoundParameters.ContainsKey('Private')) {
+            $body.private = [bool]$Private;
+        }
+        if ($PSBoundParameters.ContainsKey('DisableIssues')) {
+            $body.has_issues = -not $DisableIssues;
+        }
+        if ($DefaultBranch) {
+            $body.default_branch = $DefaultBranch
+        }
+
+        $ApiCall = @{
+            Uri = 'repos/{0}/{1}' -f $Owner, $Name;
+            Body = $body | ConvertTo-Json;
+            Method = 'Patch';
+            Token = $Token
+            BaseUri = $BaseUri
+        }
+
+        $shouldProcessCaption = "Updating GitHub repository"
+        $shouldProcessDescription = "Updating GitHub repository `e[1m$Owner/$RepositoryName`e[0m."
+        $shouldProcessWarning = "Do you want to update the GitHub repository `e[1m$Owner/$RepositoryName`e[0m?"
+
+        if ($PSCmdlet.ShouldProcess($shouldProcessDescription, $shouldProcessWarning, $shouldProcessCaption)) {
+            Invoke-GitHubApi @ApiCall | ForEach-Object {
+                $_.PSTypeNames.Insert(0, 'PSGitHub.Repository')
+                $_.Owner.PSTypeNames.Insert(0, 'PSGitHub.User')
+                if ($_.License) {
+                    $_.License.PSTypeNames.Insert(0, 'PSGitHub.License')
+                }
+                $_
+            }
+        }
     }
-    Invoke-GitHubApi @ApiCall;
 }
 
 Export-ModuleMember -Alias (
